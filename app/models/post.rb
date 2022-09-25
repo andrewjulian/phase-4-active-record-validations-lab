108d9acdbec44330ee733547de33e3ff8d70ccd0
+ <+>
2022-09-25 11:50:40 -0600

    commit tree 4f7671e0e66c092357dfd9c25012425587be043c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+
+  validate :not_true_facts
+
+  def not_true_facts
+    if title == "True Facts"
+      errors.add(:title, "Not true facts")
+    end
+  end
+
 end
